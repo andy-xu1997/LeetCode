01b7d83806a2e8f508922b7cd300367c2a2ea253
dell taishi <1278338535@qq.com>
2021-10-14 21:05:27 +0800

    尝试提交

--- main.py
+++ main.py
@@ -2,12 +2,4 @@ res = []
 n = 3
 right = 0
 left = 0
-def huisu(paths,right,left):
-    if right > n or left > right:
-        return
-    if len(paths) == 2 * n:
-        res.append(paths)
-        print(paths)
-    huisu(paths + '(', right + 1, left)
-    huisu(paths + ')', right, left + 1)
-huisu('',0,0)
+print("test")
