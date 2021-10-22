6f6402100b311df6f6ebb6e0e070640278bbb772
dell taishi <1278338535@qq.com>
2021-10-22 23:38:39 +0800

    修改 第三次

--- 外观数组  中等.py
+++ 外观数组  中等.py
@@ -3,12 +3,20 @@
 
 n = 112233445566
 def countAndSay(n):
-    array = []
-    length = len(str(n))
-    count = 0
-    while (n != 0 ):
-        t = n // (10 ** (length-1))
+    array = [] #数组 用来记录算出的结果 最后输出
+    length = len(str(n)) #输入数字的长度
+    count = 0 #用来计数
+    while (n != 0 ):    #当数字没有递减到0 的时候程序一直运行
+        t = n // (10 ** (length-1)) # t 是用来记录现在最高位的数字 并且跟之前的最高位进行判断
         count +=1
+
+        if
+        array.append(t)
+        n = n - t *(10 ** (length-1))
+        length -=1
+
+    print(array)
+
 
 
 
