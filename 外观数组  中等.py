055abddb986e58f2f1a98db4b5b7e1ece8a88f6b
dell taishi <1278338535@qq.com>
2021-10-22 23:17:07 +0800

    外观数组 第二次未完成提交

--- 外观数组  中等.py
+++ 外观数组  中等.py
@@ -1,8 +1,15 @@
+#分析 ：想用数学的方式来解决这个问题 获取到输入的数字后 分析数字的长度length 之后
+
+
 n = 112233445566
 def countAndSay(n):
     array = []
     length = len(str(n))
-    for i in range(length):
+    count = 0
+    while (n != 0 ):
+        t = n // (10 ** (length-1))
+        count +=1
+
 
 
 countAndSay(n)
